05d9d882a08a31425a2d078d793a200d598435ca
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-03-11 18:10:51 +0530

    fix: Metadata generation for Root(https://github.com/AxLabs/grantshares-docs/issues/20)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,39 +1,37 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const { themes } = require('prism-react-renderer');
+const { themes } = require("prism-react-renderer");
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'GrantShares',
-  tagline: 'A decentralized and transparent grants program for the Neo ecosystem',
-  url: 'https://docs.grantshares.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'AxLabs',
-  projectName: 'grantshares-docs',
-  deploymentBranch: 'gh-pages',
+  title: "GrantShares",
+  tagline:
+    "A decentralized and transparent grants program for the Neo ecosystem",
+  url: "https://docs.grantshares.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "AxLabs",
+  projectName: "grantshares-docs",
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
-  plugins: [ ],
+  plugins: [],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/axlabs/grantshares-docs/tree/main/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/axlabs/grantshares-docs/tree/main/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
-          exclude: [
-            '**/5_roadmap.md',
-            '**/3_Implementation/*',
-          ],
+          exclude: ["**/5_roadmap.md", "**/3_Implementation/*"],
           // While the `exclude` above excludes from the docusaurus build in general,
           // the `sidebarItemsGenerator` below allows us to create a custom function to include/exclude
           // from the sidebar.
@@ -45,31 +43,31 @@ const config = {
             const newItems = sidebarItems.filter((item) => {
               const newSubItems = item.items.filter((item) => {
                 // remove an item with a specific label 'rfp-docs' if the item is not null and the item has a label
-                return item.label !== 'rfp-docs';
+                return item.label !== "rfp-docs";
               });
               item.items = newSubItems;
               return item;
             });
-            return newItems
-          }
+            return newItems;
+          },
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/axlabs/grantshares-docs/tree/main/',
+          editUrl: "https://github.com/axlabs/grantshares-docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        }
-      }
-    ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
   ],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
   themes: [
@@ -78,9 +76,9 @@ const config = {
       require.resolve("@easyops-cn/docusaurus-search-local"),
       {
         hashed: true,
-        docsDir: "docs", 
+        docsDir: "docs",
         docsRouteBasePath: "/docs",
-        indexDocs:true,
+        indexDocs: true,
         indexPages: true,
         highlightSearchTermsOnTargetPage: true,
       },
@@ -89,18 +87,42 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/logo.svg',
+      metadata: [
+        {
+          name: "description",
+          content: "Documentation Portal for the GrantShares DAO.",
+        },
+        {
+          property: "og:title",
+          content: "Docs Portal | GrantShares",
+        },
+        {
+          property: "og:description",
+          content: "Documentation Portal for the GrantShares DAO.",
+        },
+        { property: "og:type", content: "website" },
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          name: "twitter:title",
+          content: "Docs Portal | GrantShares",
+        },
+        {
+          name: "twitter:description",
+          content: "Documentation Portal for the GrantShares DAO.",
+        },
+      ],
+      image: "img/grantshares.jpeg",
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       navbar: {
-        title: '',
-        style: 'primary',
+        title: "",
+        style: "primary",
         logo: {
-          alt: 'GrantShares Logo',
-          src: 'img/logo_and_title.png',
+          alt: "GrantShares Logo",
+          src: "img/logo_and_title.png",
         },
         items: [
           // {
@@ -110,42 +132,42 @@ const config = {
           //   label: 'Docs',
           // },
           {
-            href: 'https://grantshares.io/app/proposals',
-            label: 'Application',
-            position: 'right',
+            href: "https://grantshares.io/app/proposals",
+            label: "Application",
+            position: "right",
           },
           {
-            type: 'search',
-            position: 'right',
-          }
+            type: "search",
+            position: "right",
+          },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/rvZFQ5382k',
+                label: "Discord",
+                href: "https://discord.gg/rvZFQ5382k",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/GrantShares',
+                label: "Twitter",
+                href: "https://twitter.com/GrantShares",
               },
             ],
           },
           {
-            title: 'Links',
+            title: "Links",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/axlabs/grantshares',
+                label: "GitHub",
+                href: "https://github.com/axlabs/grantshares",
               },
               {
-                label: 'Application',
-                href: 'https://grantshares.io/app/proposals',
+                label: "Application",
+                href: "https://grantshares.io/app/proposals",
               },
             ],
           },
